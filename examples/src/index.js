11182c566c3b9bca6c3c0dd225bcf6b7a1b53ca4
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-04-14 14:17:54 +0430

    Add "build" script

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { DatePicker } from '../../src';
+import { DatePicker } from '../../lib';
 import * as serviceWorker from './serviceWorker';
 
 const App = () => {
