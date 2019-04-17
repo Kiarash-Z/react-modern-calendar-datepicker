0cdd98289a33c94a295b0d4bc187f55536653e0e
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-04-17 20:57:28 +0430

    Add SVG plugin

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -17,6 +17,14 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
