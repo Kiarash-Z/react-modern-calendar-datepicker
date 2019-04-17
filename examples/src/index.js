7cec3901cdb95219b143b8285d4f0b5b303c92cc
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-04-17 21:43:22 +0430

    Add some package specific fields

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../../lib/DatePicker.css';
-import { DatePicker } from '../../lib';
+import '../../src/DatePicker.css';
+import { DatePicker } from '../../src';
 import * as serviceWorker from './serviceWorker';
 
 const App = () => {
