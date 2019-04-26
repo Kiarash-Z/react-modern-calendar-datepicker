cc93c5bc221cfd8b43640a653d780ff960eb8ac6
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-04-26 18:10:11 +0430

    Add default-values docs screen

---
+++ website/src/pages/docs/default-values.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Link } from 'gatsby';
+
+import Docs from '../../containers/docs';
+// import { Code } from '../../components';
+
+const DefaultValues = () => {
+  return (
+    <Docs title="Default Values">
+
+    </Docs>
+  );
+};
+
+export default DefaultValues;
