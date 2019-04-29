7ad3f55451189c65230cb379084dc593ede196c6
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-04-29 10:26:29 +0430

    Enhance disabled-days page's content

--- website/src/pages/docs/disabled-days.js
+++ website/src/pages/docs/disabled-days.js
@@ -73,8 +73,10 @@ PropTypes.arrayOf(PropTypes.shape({
       <p className="Docs__paragraph">
         By passing disabled days, users won&#39;t be able to select a disabled day
         or include one in a range. At such times, <code className="custom-code">onDisabledDayError</code>
-        function will be called, and you can show a message to user. Here are some examples:
+        function will be called, and you can show a message to user.
       </p>
+
+      <h2 className="Docs__titleSecondary">Examples</h2>
 
       <div className="Docs__sampleContainer">
         <Code language="javascript">
