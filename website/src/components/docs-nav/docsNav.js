e4f43f39d7b99f1551dd1be4680ea11aef56505c
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-07-09 10:28:02 +0430

    Enhance documents

--- website/src/components/docs-nav/docsNav.js
+++ website/src/components/docs-nav/docsNav.js
@@ -1,45 +1,32 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import { TUTORIAL_ITEMS, API_ITEMS } from '../../constants/docsConstants';
+import { DOCS_NAVBAR_ITEMS, } from '../../constants/docsConstants';
 
 import styles from './docsNav.module.css';
 
 const DocsNav = () => {
   return (
     <aside className={styles.DocsNav}>
-      <div className={styles.DocsNav__section}>
-        <span className={styles.DocsNav__sectionTitle}>Tutorial</span>
-        <ul className={styles.DocsNav__list}>
-          {TUTORIAL_ITEMS.map(({ id, path, text }) => (
-            <Link
-              key={id}
-              to={path}
-              className={styles.DocsNav__listItem}
-              activeClassName={styles.Active}
-            >
-              {text}
-            </Link>
-          ))}
-        </ul>
-      </div>
-      <div className={styles.DocsNav__section}>
-        <span className={styles.DocsNav__sectionTitle}>API Reference</span>
-        <ul className={styles.DocsNav__list}>
-          {API_ITEMS.map(({ id, path, text }) => (
-            <Link
-              key={id}
-              to={path}
-              className={styles.DocsNav__listItem}
-              activeClassName={styles.Active}
-            >
-              {text}
-            </Link>
-          ))}
-        </ul>
-      </div>
+      {DOCS_NAVBAR_ITEMS.map(({ id, items, name }) => (
+        <div key={id} className={styles.DocsNav__section}>
+          <span className={styles.DocsNav__sectionTitle}>{name}</span>
+          <ul className={styles.DocsNav__list}>
+            {items.map(({ id: itemId, path, name: itemName }) => (
+              <Link
+                key={itemId}
+                to={path}
+                className={styles.DocsNav__listItem}
+                activeClassName={styles.Active}
+              >
+                {itemName}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      ))}
     </aside>
   );
-}
+};
 
 export { DocsNav };
