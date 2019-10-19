d2df95b76d969ea950da0a6dc78643303622ebc8
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-10-19 11:32:54 +0330

    Updates

--- component---src-pages-404-js-f2852f62386028bc85d2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{153:function(e,t,n){"use strict";n.r(t);var o=n(0),a=n.n(o),c=n(167);n(154);t.default=function(){return a.a.createElement(c.c,null,a.a.createElement(c.d,{title:"404: Not found"}),a.a.createElement("div",{className:"NotFound"},a.a.createElement("h1",{className:"NotFound__title"},"Ooooops..."),a.a.createElement("p",{className:"NotFound__description"},"The page you're requesting not found.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-f2852f62386028bc85d2.js.map
