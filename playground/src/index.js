2e3867f89c56b325b4889f8f6dba103e112a2979
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-10-21 13:57:13 +0330

    Add test:coverage script

--- playground/src/index.js
+++ playground/src/index.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import '../../src/DatePicker.css';
-import DatePicker from '../../src';
+import { Calendar } from '../../src';
 import * as serviceWorker from './serviceWorker';
 
 const App = () => {
   const [selectedDay, setValue] = useState(null);
-  return <DatePicker value={selectedDay} onChange={setValue} shouldHighlightWeekends />;
+  return <Calendar value={selectedDay} onChange={setValue} shouldHighlightWeekends />;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- test/datepicker-visibility.test.js
+++ test/datepicker-visibility.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
-import DatePicker from '../src/DatePicker';
+import DatePicker from '../src';
 
 describe('DatePicker Visibility', () => {
   test('toggles correctly on input focus/blur', () => {
