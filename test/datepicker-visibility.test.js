964fdfa06adbfe7e19fce288f9c123f070cae006
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-10-21 11:09:31 +0330

    Add more visibility tests for date picker

--- test/datepicker-visibility.test.js
+++ test/datepicker-visibility.test.js
@@ -3,7 +3,7 @@ import { render, fireEvent } from '@testing-library/react';
 
 import DatePicker from '../src/DatePicker';
 
-describe('DatePicker', () => {
+describe('DatePicker Visibility', () => {
   test('toggles correctly on input focus/blur', () => {
     const { container, getByTestId } = render(<DatePicker />);
     const calendarContainer = getByTestId('calendar-container');
@@ -40,5 +40,59 @@ describe('DatePicker', () => {
     Object.defineProperty(document.documentElement, 'clientWidth', { value: 100 });
     fireEvent.focus(input);
     // TODO: complete test (https://github.com/testing-library/dom-testing-library/issues/387)
+  });
+
+  test('closes calendar on single day selection', () => {
+    const { container, getAllByText, getByTestId } = render(
+      <DatePicker value={null} onChange={() => {}} />,
+    );
+    const monthDay = getAllByText('1')[0];
+    const input = getByTestId('datepicker-input');
+    input.focus();
+    fireEvent.click(monthDay);
+    expect(container.firstChild).not.toHaveClass('-calendarOpen');
+  });
+
+  test('closes calendar on day range selection', () => {
+    const { container, getAllByText, getByTestId, rerender } = render(
+      <DatePicker value={{ from: null, to: null }} onChange={() => {}} />,
+    );
+    const input = getByTestId('datepicker-input');
+    const rangeStart = getAllByText('1')[0];
+    const rangeEnd = getAllByText('5')[0];
+    input.focus();
+
+    fireEvent.click(rangeStart);
+    rerender(
+      <DatePicker
+        value={{ from: { year: 2019, month: 10, day: 21 }, to: null }}
+        onChange={() => {}}
+      />,
+    );
+
+    // calendar still open
+    expect(container.firstChild).toHaveClass('-calendarOpen');
+
+    fireEvent.click(rangeEnd);
+
+    // now closed
+    expect(container.firstChild).not.toHaveClass('-calendarOpen');
+  });
+
+  test('keeps calendar open on multi day selection', () => {
+    const { container, getAllByText, getByTestId } = render(
+      <DatePicker value={[]} onChange={() => {}} />,
+    );
+    const input = getByTestId('datepicker-input');
+    const day1 = getAllByText('1')[0];
+    const day2 = getAllByText('5')[0];
+    const day3 = getAllByText('10')[0];
+    input.focus();
+
+    fireEvent.click(day1);
+    fireEvent.click(day2);
+    fireEvent.click(day3);
+
+    expect(container.firstChild).toHaveClass('-calendarOpen');
   });
 });
